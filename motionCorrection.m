84bea4b2fa2337f5281de059461cab43dff28bd5
Vinicius Ambrosi <42151837+vambrosi@users.noreply.github.com>
2025-05-06 15:28:35 -0700

    Transform motionCorrection into a function

--- motionCorrection.m
+++ motionCorrection.m
@@ -1,23 +1,30 @@
-% MOTIONCORRECTION Apply motion correction to all tiff stacks in a folder.
-% Uses the NoRMCorre non-linear algorithm with default settings. Saves the
-% resulting motion corrected stacks in a separate folder.
+function motionCorrection(experimentFolder, appendToImages)
+% MOTIONCORRECTION - Applies motion correction to '.tif' files in the
+% folder experimentFolder/raw. Saves motion corrected images and metadata
+% to experimentFolder/processed/mcor (creating this folder if needed).
+%
+% Syntax
+%   MOTIONCORRECTION(experimentFolder)
+%   MOTIONCORRECTION(experimentFolder, appendToImages)
+%
+% Input Arguments
+%   experimentFolder - Path to the experiment folder
+%       string scalar | character vector
+%   appendToImages - Text to add at the end of filenames
+%       '_mcor' (default) | string scalar | character vector
 
-%% User Input (folder with images to motion correct)
+arguments
+   experimentFolder {mustBeFolder}
+   appendToImages char = '_mcor'
+end
 
-experimentFolder = '../OdorEvokedDynamicsData/20250321/Experiment1';
-
-% Default folder settings
+% Assumes the default folder structure for a experiment
 imagesFolder = fullfile(experimentFolder, 'raw');
 saveFolder = fullfile(experimentFolder, 'processed', 'mcor');
-appendToImages = '_mcor';
 
-%% Motion Correction
-
-% Start parallel processing for later
-gcp;
-
-% Store file names
+% Store file paths
 files = dir(fullfile(imagesFolder, "*.tif"));
+if isempty(files), error("Image folder is empty."); end
 
 % Make sure they are in the right order
 [~, ind] = sort({files.name});
@@ -29,11 +36,10 @@ if ~isfolder(saveFolder)
 end
 
 % Open first frame of the first file to get dimensions
-assert(~isempty(files), "Image folder is empty.");
 firstFrame = read_file(fullfile(files(1).folder, files(1).name), 1, 1);
 
-% Set options for the motion correction algorithm.
-options = NoRMCorreSetParms( ...
+% Set parameters for the motion correction algorithm
+parameters = NoRMCorreSetParms( ...
     'd1', size(firstFrame, 1), ...
     'd2', size(firstFrame, 2), ...
     'grid_size', [32,32], ...
@@ -47,13 +53,16 @@ options = NoRMCorreSetParms( ...
 );
 
 % Run motion correction of file using template of the last file
-% See github.com/flatironinstitute/NoRMCorre/issues/12 for details.
+% See github.com/flatironinstitute/NoRMCorre/issues/12 for details
 
 % File path where current state will be saved 
 [~, filename1, ~] = fileparts(files(1).name);
 [~, filename2, ~] = fileparts(files(end).name);
 stateFilename = [filename1 '_' filename2(end-4:end)];
 statePath = fullfile(saveFolder, [stateFilename '.mat']);
+
+% Start parallel processing
+gcp;
 
 template = [];
 for i = 1:size(files, 1)
@@ -66,17 +75,19 @@ for i = 1:size(files, 1)
 
     % Set destination path to motion corrected file
     mcorPath = fullfile(saveFolder, [filename appendToImages extension]);
-    options.tiff_filename = mcorPath;
+    parameters.tiff_filename = mcorPath;
     
     % Run motion correction algorithm
     tic;
-    [~, ~, template, options] = ...
-        normcorre_batch(filePath, options, template);
+    [~, ~, template, parameters] = ...
+        normcorre_batch(filePath, parameters, template);
     toc
 
     % Save current state in case loop is interrupted
     % - filename tells where the loop stopped
     % - template is needed to keep motion correction consistent
-    % - options stores the initial settings
-    save(statePath, 'filename', 'template', 'options');
+    % - parameters stores the initial settings
+    save(statePath, 'filename', 'template', 'parameters');
+end
+
 end
